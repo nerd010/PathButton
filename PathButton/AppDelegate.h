862438ff7693ed6434f7b43872f0325da9006fb6
王增康 <Charles@wangzengkangdemacbook-pro.local>
2016-01-05 22:06:17 +0800

    Initial Commit

---
+++ PathButton/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PathButton
+//
+//  Created by 王增康 on 16/1/5.
+//  Copyright © 2016年 CHW. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PathButton/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PathButton
+//
+//  Created by 王增康 on 16/1/5.
+//  Copyright © 2016年 CHW. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
